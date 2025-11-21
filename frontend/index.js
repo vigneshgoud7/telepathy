e4fbdfa105e73795775d967cc71090db97e79cd9
Sai prashanthi <Saiprashanthivangaveeti@gmail.com>
2025-11-22 05:26:49 +0530

    ahahaha

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,4 @@
-const socket = io()
+// const socket = io()
 const user = localStorage.getItem("telepathy_user")
 let disConnectBtnEl=document.querySelector("#disconnect-btn")
 disConnectBtnEl.addEventListener("click",()=>{
@@ -16,4 +16,4 @@ sendEl.addEventListener("click",()=>{
     chatArea.append(divEl)
     document.getElementById("msg-input").value=""
 })
-let 
+// let
--- frontend/script.js
+++ frontend/script.js
@@ -1,17 +1,17 @@
-let buttonEl=document.getElementById("connect-btn")
-buttonEl.addEventListener("click",()=>{
-     const name = document.getElementById("username-input").value.trim();
-            if (!name) {
-                document.getElementById("login-error").innerText = "Codename cannot be empty.";
-                document.getElementById("login-error").classList.remove("hidden");
-                return;
-            }
-            localStorage.setItem("telepathy_user", name);
-    window.location.href="dashboard.html"
-})
+    let buttonEl=document.getElementById("connect-btn")
+    buttonEl.addEventListener("click",()=>{
+        const name = document.getElementById("username-input").value.trim();
+                if (!name) {
+                    document.getElementById("login-error").innerText = "Codename cannot be empty.";
+                    document.getElementById("login-error").classList.remove("hidden");
+                    return;
+                }
+                localStorage.setItem("telepathy_user", name);
+        window.location.href="dashboard.html"
+    })
 
 
-// function logout() {
-//         localStorage.removeItem("telepathy_user");
-//         window.location.href = "login.html";
-//     }
+    // function logout() {
+    //         localStorage.removeItem("telepathy_user");
+    //         window.location.href = "login.html";
+    //     }
